e77c24dffe865611e938d07d46a2b20e19a057a5
Behnood Eghbali <behnood-eghbali@users.noreply.github.com>
2020-10-17 10:45:03 +0330

    Update DataCard.js

--- src/DataCard/DataCard.js
+++ src/DataCard/DataCard.js
@@ -2,6 +2,10 @@ import React, { useContext } from 'react'
 import { InputContext } from '../Contexts/InputContext'
 import Main from './Main'
 import Title from './Title'
+import Bio from './Bio'
+import Work from './Work'
+import Skill from './Skill'
+import College from './College'
 
 export default function DataCard() {
 
@@ -9,6 +13,10 @@ export default function DataCard() {
         <div>
             <Title />
             <Main />
+            <Bio />
+            <Work />
+            <Skill />
+            <College />
         </div>
     )
 }
